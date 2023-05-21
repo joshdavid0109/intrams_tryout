59b8a679c459efc5881c1b9245625a7a20b5a0d2
darren franz e domantay <2225292@slu.edu.ph>
2023-05-21 16:04:28 +0800

    kumit baka may magalaw ako

--- src/main/java/org/gui/controllers/AdminAssignController.java
+++ src/main/java/org/gui/controllers/AdminAssignController.java
@@ -1,4 +1,32 @@
 package org.gui.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import org.gui.objects.Sport;
+
+
 public class AdminAssignController {
+
+    @FXML
+    public TextField coachNo;
+
+    @FXML
+    public TextField SportsCode;
+
+    @FXML
+    public Button assignSportsBTN;
+
+    @FXML
+    public Button cancelAssignSportsBTN;
+
+    public void assignSport(ActionEvent event) {
+        System.out.println(coachNo.getText());
+        System.out.println(SportsCode.getText());
+    }
+
+    public void cancelSport(ActionEvent event) {
+        coachNo.setText("");
+        SportsCode.setText("");
+    }
 }
